73abf681acf3da315e04a14a35e650abfb539119
ha17142 <ha17142@tomakomai.kosen-ac.jp>
2021-09-10 12:24:55 +0900

    コマンドライン引数でラベル名のファイルを与えられるようにした

--- select.py
+++ select.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import sys
 
 TARGETLABELS=[
     'Speech',
@@ -10,12 +11,25 @@ TARGETLABELS=[
 
 ]
 
+DIR = os.path.dirname(os.path.abspath(__file__))
+print(DIR)
+
 def main():
+    argv=sys.argv
+    if(len(argv) > 1):
+        target = ReadInputfile(argv[1])
+        print(target)
+        labels = SelectLabels(target)
+    else:
+        labels=SelectLabels(TARGETLABELS)
     filename=SelectFilename()
-    labels=SelectLabels(TARGETLABELS)
+    
 
     CreateFiles(labels, filename, 50)
     print('終了しました!!!')
+
+
+
 #labels:['作成するファイル名','ラベルの実態']の配列
 #filename:選別したいファイル名
 #num:ファイルに書き込む数
@@ -23,25 +37,25 @@ def CreateFiles(labels, filename, num=1000):
     for label in labels:
         result=list()
 
-        with open('target/' + filename) as f:
+        with open(DIR+'/target/' + filename) as f:
             
             for line in f.readlines():
                 if (line.find(label[1]) >= 0):
                     result.append(line)
-        with open('result/'+label[0]+'.csv', mode='w') as f:
+        with open(DIR+'/result/'+label[0]+'.csv', mode='w') as f:
             f.writelines(result[0 : num])
 
-#targetの要素と等しい列のラベルをCreateFileに流せる形式で返す
+#配列targetの要素をラベルに含む行のリストをCreateFileに流せる形式で返す
 def SelectLabels(target):
     result=list()
-    with open('target/class_labels_indices.csv') as f:
+    with open(DIR+'/target/class_labels_indices.csv') as f:
         lines=csv.reader(f, delimiter=',', doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
         
         for line in lines:
             
             if(line[2] in target):
                 result.append(line[3:0:-1])
-    with open('result/labels.csv', mode='w') as f:
+    with open(DIR+'/result/labels.csv', mode='w') as f:
         for i in result:
             f.write(','.join(i) + '\n')
     return result
@@ -63,7 +77,12 @@ def SelectFilename():
     finally:
         return r
 
-
+def ReadInputfile(argv):
+    r=[]
+    with open(argv) as f:
+        for i in f.read().splitlines():
+            r.append(i)
+    return r
 
 
 
